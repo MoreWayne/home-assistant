f628dfd38bf729b229785b21eb5380062f966853
gongshuai <923554473@qq.com>
2023-12-10 14:32:38 +0800

    init

---
+++ src/main/java/top/gongshuai/HomeAssistantApplication.java
@@ -0,0 +1,13 @@
+package top.gongshuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeAssistantApplication.class, args);
+    }
+
+}
