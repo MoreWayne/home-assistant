94a8338c59dedd428e4e86a85894cbc5d00eefaf
gongshuai <923554473@qq.com>
2024-04-12 20:22:11 +0800

    init

--- src/main/java/top/gongshuai/homeassistant/controller/TestController.java
+++ src/main/java/top/gongshuai/homeassistant/controller/TestController.java
@@ -1,4 +1,4 @@
-package top.gongshuai.homeassistant;
+package top.gongshuai.homeassistant.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
