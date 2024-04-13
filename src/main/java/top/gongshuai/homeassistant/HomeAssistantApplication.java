4bc2ba2df74b39b16d9191ee084915de967a7214
gongshuai <923554473@qq.com>
2024-04-13 10:45:53 +0800

    init

--- src/main/java/top/gongshuai/homeassistant/HomeAssistantApplication.java
+++ src/main/java/top/gongshuai/homeassistant/HomeAssistantApplication.java
@@ -1,9 +1,13 @@
 package top.gongshuai.homeassistant;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("top.gongshuai.homeassistant.dal.mapper")
+@EnableScheduling
 public class HomeAssistantApplication {
 
     public static void main(String[] args) {
