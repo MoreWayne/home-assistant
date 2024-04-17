3376ce98881a173232fbf5d643fb3744e51e3aaa
gongshuai <923554473@qq.com>
2024-04-17 22:52:43 +0800

    init

---
+++ src/main/java/top/gongshuai/homeassistant/dto/common/Result.java
@@ -0,0 +1,25 @@
+package top.gongshuai.homeassistant.dto.common;
+
+import lombok.Data;
+import org.apache.http.HttpStatus;
+
+@Data
+public class Result<T> {
+
+    private  String code;
+
+    private  String msg;
+
+    private  T data;
+
+    private Result(){}
+
+    public  Result<T> createSuccessResult(String code, String msg, T data) {
+        Result<T> result = new Result<T>();
+        result.setCode(String.valueOf(HttpStatus.SC_OK));
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+
+}
