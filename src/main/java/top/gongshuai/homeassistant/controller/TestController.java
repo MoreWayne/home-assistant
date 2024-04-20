28bcde400ab88fb19ed14ea49ec5b39e13535616
gongshuai <923554473@qq.com>
2024-04-20 23:33:18 +0800

    init

--- src/main/java/top/gongshuai/homeassistant/controller/TestController.java
+++ src/main/java/top/gongshuai/homeassistant/controller/TestController.java
@@ -1,11 +1,13 @@
 package top.gongshuai.homeassistant.controller;
 
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.gongshuai.homeassistant.dal.mapper.UserMapper;
 
 @RestController
+@Slf4j
 public class TestController {
     @Resource
     private UserMapper userMapper;
